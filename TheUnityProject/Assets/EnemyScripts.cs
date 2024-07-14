9aec48ce26bb5fc061d045bfe9ceca55173a3b7d
Deltager <teknisk+GDC2024-GR3@game.unf.dk>
2024-07-14 14:28:16 +0200

    Added enemy AI og Enemy AI

--- TheUnityProject/Assets/EnemyScripts.cs
+++ TheUnityProject/Assets/EnemyScripts.cs
@@ -2,19 +2,24 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyScripts : MonoBehaviour
 {
+    public NavMeshAgent agent;
+
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player");
+        agent.SetDestination(player.transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(player.transform.position);
     }
 
     private void OnCollisionEnter(Collision  other)
