9ceb4a6e806bdb45830f2f9ad18c7a68c490a4d4
Deltager <teknisk+GDC2024-GR3@game.unf.dk>
2024-07-16 14:05:11 +0200

    Added titlescreen and added small demo

---
+++ TheUnityProject/Assets/Scripts/ArenaButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ArenaButton : MonoBehaviour
+{
+    public void StartArena()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    
+}
